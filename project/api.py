18367ed74836d41d679dcf5c3aa189d0938f904b
Thomas <thomas@unknown00e44f680746.localdomain>
2021-10-05 21:03:57 +0200

    first code

---
+++ project/api.py
@@ -0,0 +1,55 @@
+import flask
+from flask import request, jsonify
+import numpy as np
+import time
+import sys
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+mem = list()
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify("Distant Reading Archive. This site is a "
+                   "prototype API for distant reading of science fiction novels.")
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify("OK")
+
+
+@app.route('/cpu', methods=['GET'])
+def cpu():
+
+    k = 0
+    for i in range(1000000):
+        k += np.sqrt(i)
+
+    return jsonify(k)
+
+
+@app.route('/memory', methods=['GET'])
+def memory():
+
+    global mem
+
+    a = "Hello Wooooooooooooooooooooooooooooooooooooooooooooooooooooorld" * 10000000
+
+    mem.append(a)
+
+    return jsonify("Done")
+
+
+@app.route("/length", methods=["GET"])
+def mem_get():
+
+    global mem
+
+    return jsonify(sys.getsizeof(mem))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
